b90bde4e9a3571659f60c3bab7a7e40562259d9e
GoJiMin <gojimin3095@gmail.com>
2025-02-17 16:17:36 +0900

    ch/01 파일별 배포
    - 서버에서 제공할 파일을 직접 변수로 제공하는 것은 적절한 처리 방법이 아니다. 파일이 변경될 때마다 수정하거나 혹은 변경될 때마다 서버를 다시 실행시켜야만 하니까.
    - 이제 제공할 파일을 public 폴더에 txt 형태로 저장하고 제공한다.
    - 기존의 handler 함수의 역할을 static 함수로 위임한다. static 함수는 동일하게 요청 객체와 응답 객체를 인자로 받는다. path 모듈의 join 함수를 사용하면 여러 경로를 조합해 하나의 경로로 만들 수 있다.
    - __dirname은 현재 경로를 나타내며, public은 문서가 저장된 파일 경로, 그리고 req 객체의 url 속성은 요청 주소를 나타내는데 이를 조합해 filepath라는 경로를 생성한다.
    - fs 모듈을 사용해 파일을 읽을 수 있다. 파일을 읽은 I/O 작업은 비동기 작업으로 콜백 함수가 두 번째 인자로 제공된다. 파일을 읽지 못했다면 err 객체가 전달되니 사용자에게 찾지 못했다는 응답을 전송한 뒤 요청을 종료한다. 반대로 파일을 찾은 경우 해당 파일을 클라이언트에게 전송하고 요청을 종료한다.
    
    - 클라이언트 측에서 요청 URL을 하드코딩하지 않고, 서버의 요청 파일을 커맨드라인으로 입력 받아 생성한다.

--- ch01/my-client/index.js
+++ ch01/my-client/index.js
@@ -1,27 +1,54 @@
 // 마찬가지로 클라이언트 환경에도 http 모듈을 가져온다.
 const http = require("http");
 
-// 클라이언트에서 요청할 서버의 URL 객체를 생성한다.
-const options = new URL("http://localhost:3000/");
+// // 클라이언트에서 요청할 서버의 URL 객체를 생성한다.
+// const options = new URL("http://localhost:3000/");
 
-// 클라이언트 측에서 서버의 응답을 처리할 handler 함수를 정의한다. 응답 객체인 res 객체가 들어온다.
+// // 클라이언트 측에서 서버의 응답을 처리할 handler 함수를 정의한다. 응답 객체인 res 객체가 들어온다.
+// const handler = (res) => {
+//   const data = [];
+
+//   // res 객체의 data 이벤트를 구독한다.
+//   // 서버의 응답 데이터가 chunk로 들어오는데, 문자열로 변경해 data 배열에 추가한다.
+//   res.on("data", (chunk) => {
+//     data.push(chunk.toString());
+//   });
+
+//   // 응답이 종료될 때, end 이벤트가 발행되는데 그동안 쌓인 데이터를 하나로 모아 콘솔에 출력한다.
+//   res.on("end", () => {
+//     console.log(data.join(""));
+//   });
+// };
+
+// // request 함수를 통해 요청 객체를 만든다. URL 객체와 handler 함수를 등록한다.
+// const req = http.request(options, handler);
+
+// // end 함수를 호출해 서버에 요청을 보낸다.
+// req.end();
+
+// 노드의 process 객체의 argv 객체 배열에는 노드 플랫폼에서 실행될 때 사용한 커맨드라인 명령어에 접근이 가능하다. 2번 인덱스에 프로그램 경로 이후에 전달된 인자 값을 문자열로 받을 수 있다.
+// node my-client/ch01 <여기>
+const url = process.argv[2];
+
+if (!url) {
+  console.error(`Usage: node my-client <요청할 파일 이름>`);
+  process.exit();
+}
+
+// 이제 요청할 파일별로 요청 URL 객체를 생성한다.
+const options = new URL(`http://localhost:3000/${url}`);
+
 const handler = (res) => {
   const data = [];
 
-  // res 객체의 data 이벤트를 구독한다.
-  // 서버의 응답 데이터가 chunk로 들어오는데, 문자열로 변경해 data 배열에 추가한다.
   res.on("data", (chunk) => {
     data.push(chunk.toString());
   });
 
-  // 응답이 종료될 때, end 이벤트가 발행되는데 그동안 쌓인 데이터를 하나로 모아 콘솔에 출력한다.
   res.on("end", () => {
     console.log(data.join(""));
   });
 };
 
-// request 함수를 통해 요청 객체를 만든다. URL 객체와 handler 함수를 등록한다.
 const req = http.request(options, handler);
-
-// end 함수를 호출해 서버에 요청을 보낸다.
 req.end();
--- ch01/my-server/index.js
+++ ch01/my-server/index.js
@@ -1,25 +1,58 @@
 // http 모듈을 가져와 웹 서버를 만든다.
 const http = require("http");
 
-// 서버가 제공할 컨텐츠
-const content = `HTTP Lecture
-1. Basic
-  1.1 HTTP Start
-  1.2 HTTP Message
-2. Web Browser
-  2.1 Content Negotitation
-  2.2 Cookie
-`;
+// // 서버가 제공할 컨텐츠
+// const content = `HTTP Lecture
+// 1. Basic
+//   1.1 HTTP Start
+//   1.2 HTTP Message
+// 2. Web Browser
+//   2.1 Content Negotitation
+//   2.2 Cookie
+// `;
 
-const handler = (req, res) => {
-  // res 객체의 write 함수를 사용하면, 요청한 클라이언트한테 데이터를 전송할 수 있다.
-  res.write(content);
-  // end 함수로 종료 응답을 클라이언트에게 전송
-  res.end();
+// const handler = (req, res) => {
+//   // res 객체의 write 함수를 사용하면, 요청한 클라이언트한테 데이터를 전송할 수 있다.
+//   res.write(content);
+//   // end 함수로 종료 응답을 클라이언트에게 전송
+//   res.end();
+// };
+
+// // createServer로 서버 인스턴스를 만들며, 생성한 handler 함수를 전달해 요청이 들어올 때, 서버 객체가 req, res 객체를 핸들러 함수로 전달해준다.
+// const server = http.createServer(handler);
+
+// // 컴퓨터의 3000번 포트를 열어 http 요청을 기다리게 된다.
+// server.listen(3000, () => console.log("server is running ::3000"));
+
+// handler 함수의 역할을 위임할 static 함수를 정의한다.
+
+const path = require("path");
+const fs = require("fs");
+
+const static = (req, res) => {
+  // 요청 객체의 url 속성은 요청 주소를 나타낸다.
+  // path 모듈의 join 함수를 사용하면 여러 경로를 조합해 하나의 경로를 생성할 수 있다.
+  // __dirname은 현재 경로를 나타낸다 process.cwd()랑 같은듯?
+  const filepath = path.join(__dirname, "public", req.url);
+
+  // fs 모듈을 사용하면 파일을 읽을 수 있다. 파일을 읽는 I/O 작업은 비동기 작업으로 콜백 함수가 제공된다.
+  fs.readFile(filepath, (err, data) => {
+    // 파일을 읽지 못한다면 err 객체가 전달된다.
+    if (err) {
+      // 응답 객체의 write 함수를 사용해 찾지 못했다고 클라이언트 측에 데이터를 전송해 응답 종료 신호를 보내고 함수를 종료한다.
+      res.write("Not Found\n");
+      res.end();
+      return;
+    }
+
+    // 파일을 찾았다면, 데이터를 전송하고 종료 신호를 보낸다.
+    res.write(data);
+    res.end();
+  });
 };
 
-// createServer로 서버 인스턴스를 만들며, 생성한 handler 함수를 전달해 요청이 들어올 때, 서버 객체가 req, res 객체를 핸들러 함수로 전달해준다.
+const handler = (req, res) => static(req, res);
+
 const server = http.createServer(handler);
 
-// 컴퓨터의 3000번 포트를 열어 http 요청을 기다리게 된다.
 server.listen(3000, () => console.log("server is running ::3000"));
