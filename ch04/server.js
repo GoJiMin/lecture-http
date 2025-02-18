7baa81ee93c1921f0b1c49211a02c94a4784e629
GoJiMin <gojimin3095@gmail.com>
2025-02-18 17:40:50 +0900

    ch/04 쿠키로 재방문 여부 판단하기
    - HTTP 요청의 흐름으로 이해해보자. 서버는 클라이언트의 재방문 여부를 판단할 수 없었다. 지금 이 쿠키는 일종의 스티커다. 클라이언트의 재방문 여부를 판단한다고 생각하면 된다.
    - 클라이언트가 처음으로 localhost:3000에 요청을 보내면, 서버는 Set-Cookie: sid=1 헤더를 응답에 포함해 보낸다. 이후 응답을 받은 클라이언트의 브라우저는 응답 헤더의 Set-Cookie를 보고 sid=1 쿠키를 저장소에 저장한다.
    - 이후 동일한 도메인인 localhost:3000에 요청할 때 브라우저는 Cookie: sid=1 형식으로 브라우저의 쿠키 저장소에 저장된 쿠키를 요청 헤더에 포함해 전송한다.
    - 요청을 받은 서버는 req.headers['cookie']로 요청 헤더의 쿠키를 보고, 미리 부착한 스티커가 있는지 판단해 재방문 여부를 판단할 수 있게 된다.

---
+++ ch04/server.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const handler = (req, res) => {
+  const cookie = req.headers["cookie"];
+
+  if (cookie && cookie.includes("sid")) {
+    res.write("Welcome again!\n");
+    res.end();
+    return;
+  }
+
+  res.setHeader("Set-Cookie", "sid=1");
+  res.write("Welcome!\n");
+  res.end();
+};
+
+const server = http.createServer(handler);
+server.listen(3000, () => console.log("server is running ::3000"));
