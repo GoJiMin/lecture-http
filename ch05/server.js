f78b43e7b380dc34613f029d5a29473e376ca8f9
GoJiMin <gojimin3095@gmail.com>
2025-03-26 21:00:20 +0900

    ch/05 브라우저의 HTTP 요청 - img
    - 브라우저는 HTML 문서를 파싱하면서 img 태그를 만나면 해당 URL로 GET 요청을 보내 파일을 받아오려고 한다.
    - 이를 이용해 사용자의 정보를 로그화시켜 분석할 수도 있을 것이다.
    - 먼저 script.js 파일이 로딩될 때 body에 1px * 1px에 화면에 보이지 않을 img 태그를 만들어 삽입한다.
    - 그럼 스크립트가 로딩된 후 img 태그가 추가되며 브라우저는 /tracking-pixel.gif에 해당하는 GET 요청을 서버로 보내게 된다.
    - 그럼 서버는 받은 req.url에서 경로만을 떼어내 /tracking-pixel.gif에 해당하는 경로가 요청으로 들어오면 사용자의 정보를 로그로 찍을 수 있다.

--- ch05/server.js
+++ ch05/server.js
@@ -2,7 +2,22 @@ const http = require("http");
 const path = require("path");
 const static = require("../shared/serve-static");
 
+const logRequest = (req) => {
+  const log = [
+    `${new Date().toISOString()}`,
+    `IP: ${req.socket.remoteAddress || req.conection.remoteAddress}`,
+    `User-Agent: ${req.headers["user-agent"]}`,
+    `Referer: ${req.headers["referer"]}`,
+  ].join("\n");
+
+  console.log(log);
+};
+
 const handler = (req, res) => {
+  const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+
+  if (pathname === "/tracking-pixel.gif") logRequest(req);
+
   // static 함수로 위임.. 현재 파일이 위치한 폴더의 절대 경로에 public을 합쳐 인자로 전달해 함수를 리턴 받는다.
   // 리턴되는 함수로 전달된 req, res 객체를 다시 전달한다.
   static(path.join(__dirname, "public"))(req, res);
