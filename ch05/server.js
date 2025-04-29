b28d60c3dee366b846d198b4e6d6250693f635b7
GoJiMin <gojimin3095@gmail.com>
2025-04-29 18:15:20 +0900

    ch/05 브라우저의 HTTP 요청 - form
    - 특정 시점에 HTTP 요청을 만들어 서버로 보내야 하는 경우가 있다.
    - 로그인이 그 예다. 사용자가 아이디와 비밀번호를 입력한 뒤 사용자가 로그인 버튼을 클릭한 시점에
      해당 값으로 인증을 받을 수 있는 요청을 만들어야 한다.
    - 당연히 서버가 아이디와 비밀번호를 가지고 있기 때문이다.
    - html 문서에 form 태그를 만들어 특정 시점에 입력한 값을 기반으로 HTTP 요청을 만들 수 있다.
    - 크게 form 태그는 GET 요청과 POST 요청을 만들어낼 수 있다. method 속성으로 지정한 뒤 action 속성에 작성한 주소로 해당 요청을 만들어 보내게 된다.
    - GET 요청은 입력한 값이 쿼리 문자열 형태로 전달되기 때문에 서버에서 URL 객체를 생성해 파싱해 사용할 수 있다.
    - 하지만 몇 가지 제한 사항이 존재한다. 처리 가능한 URL의 최대 길이가 있어 실을 수 있는 데이터 양의 한계점이 존재한다.
      또, 주소창이나 서버 로그에 브라우저가 전달한 데이터가 기록될 수 있어 민감한 데이터를 사용하는 데에는 적합하지 않다.
    - 다음으로 POST 요청을 만들어낼 수 있다. POST 요청은 GET 요청과 달리 입력한 값을 요청 본문(body)에 담을 수 있다. 기본 요청의 경우
      Content-Type이 application/x-www-form-urlencoded로 입력한 영역과 값을 key와 인코딩한 value를 =로 묶어 한 쌍으로 만든다. 만약 값이 여러 개일 경우 &로 묶어 전달한다.
    - 하지만 이 기본 값인 application/x-www-form-urlencoded는 데이터를 인코딩하기 때문에 데이터가 많아지면 전송에 비효율적이고, 파일을 전송할 수 없는 한계점이 존재한다.
    - 이 경우 mulitipart/form-data 방식을 사용할 수 있는데 요청 본문을 여러 부분으로 나누어 전송한다.
    - 각 부분은 헤더와 바디로 구성되어 헤더 영역은 각 영역에 대한 메타 데이터를 표시하고 바디 영역에 실제 데이터가 담겨 보내진다.

--- ch05/server.js
+++ ch05/server.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const path = require("path");
+const queryString = require("querystring");
 const static = require("../shared/serve-static");
 
 const logRequest = (req) => {
@@ -13,10 +14,56 @@ const logRequest = (req) => {
   console.log(log);
 };
 
+const getLogin = (req, res) => {
+  const { searchParams } = new URL(req.url, `http://${req.headers.host}`);
+
+  const email = searchParams.get("email");
+  const password = searchParams.get("password");
+
+  const authenticated = email === "myemail" && password === "mypassword";
+
+  if (!authenticated) {
+    res.statusCode = 401;
+    res.write("Unathorized\n");
+
+    res.end();
+    return;
+  }
+
+  res.write("Login Success!");
+  res.end();
+};
+
+const postLogin = (req, res) => {
+  let body = "";
+
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+
+  req.on("end", () => {
+    const { email, password } = queryString.parse(body);
+
+    const authenticated = email === "myemail" && password === "mypassword";
+
+    if (!authenticated) {
+      res.statusCode = 401;
+      res.write("Unathorized\n");
+
+      res.end();
+      return;
+    }
+
+    res.write("Login Success!");
+    res.end();
+  });
+};
+
 const handler = (req, res) => {
   const { pathname } = new URL(req.url, `http://${req.headers.host}`);
 
   if (pathname === "/tracking-pixel.gif") logRequest(req);
+  if (pathname === "/login") return postLogin(req, res);
 
   // static 함수로 위임.. 현재 파일이 위치한 폴더의 절대 경로에 public을 합쳐 인자로 전달해 함수를 리턴 받는다.
   // 리턴되는 함수로 전달된 req, res 객체를 다시 전달한다.
