b50028424b40d953b1d616977f26ff6cf8bed923
GoJiMin <gojimin3095@gmail.com>
2025-04-29 23:33:10 +0900

    ch/06 fetch API
    - 이전 장에서 form 태그를 이용한 로그인 과정을 fetch로 변경.
    - DOM이 로드되었을 때 발행되는 이벤트에 init 함수를 등록한다.
    - init 함수 내부에서 login-form 아이디를 가진 form 태그를 찾아 submit 이벤트가 발행될 때 handleSubmit 함수가 실행되도록 이벤트를 등록한다.
    - 기본적으로 form 태그는 submit 이벤트가 발생할 때 페이지를 갱신한다. preventDefault를 호출해 기본 동작을 막는다.
    - event.target은 이벤트가 발행된 form 태그를 가리킨다. new FormData 생성자 함수를 사용해 target을 전달한다.
    - email과 password 데이터를 get 함수를 사용해 가져온다.
    - 객체에 email과 password 값을 등록한 뒤 서버로 전달할 때 객체로 전달할 수 없기 때문에 JSON.stringify를 사용해 JSON 문자열로 바꾼다.
    - JSON 문자열임을 명시하기 위해 Content-Type 헤더를 application/json로 지정해 실어보낸다.
    - 마찬가지로 서버도 JSON 문자열을 자바스크립트 객체로 파싱한 뒤 데이터를 추출해 로그인 검증을 수행한다.

--- ch06/public/script.js
+++ ch06/public/script.js
@@ -1,5 +1,43 @@
 const init = () => {
-  // Todo
+  const loginFormEl = document.querySelector("#login-form");
+
+  if (!loginFormEl) throw "no #login-form element";
+
+  loginFormEl.addEventListener("submit", handleSubmit);
+};
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  const jsonData = {
+    email,
+    password,
+  };
+
+  try {
+    const response = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jsonData),
+    });
+
+    if (!response.ok) {
+      alert("Login failed");
+
+      return;
+    }
+
+    alert("Login success");
+  } catch (error) {
+    alert("Network error:", +error.message);
+  }
 };
 
 document.addEventListener("DOMContentLoaded", init);
